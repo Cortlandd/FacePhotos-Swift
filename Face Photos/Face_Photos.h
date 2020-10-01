d29aea56394635a62e1c7f31483d29aa2ee4973b
Cortland Walker <cortlandjwalker@icloud.com>
2020-09-30 22:27:21 -0500

    Initial Commit

---
+++ Face Photos/Face_Photos.h
@@ -0,0 +1,18 @@
+//
+//  Face_Photos.h
+//  Face Photos
+//
+//  Created by Cortland Walker on 9/30/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Face_Photos.
+FOUNDATION_EXPORT double Face_PhotosVersionNumber;
+
+//! Project version string for Face_Photos.
+FOUNDATION_EXPORT const unsigned char Face_PhotosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Face_Photos/PublicHeader.h>
+
+
